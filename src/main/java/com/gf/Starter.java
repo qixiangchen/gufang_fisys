cb14d9bd692b2b7c4ca9c88f978dc6d4ca26897d
qixiang_chen@aliyun.com <qixiang_chen@aliyun.com>
2020-05-22 07:49:00 +0800

    删除注释

--- src/main/java/com/gf/Starter.java
+++ src/main/java/com/gf/Starter.java
@@ -3,16 +3,11 @@ package com.gf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//SpringBoot启动类
-//新代码
-//add by chenqx
-//新的修改2020-05-22
 @SpringBootApplication
 public class Starter 
 {
     public static void main( String[] args )
     {
-    	//Add New
         SpringApplication.run(Starter.class,args);
     }
 }
